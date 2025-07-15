a7da4bd662d91ed61a845a5b6433c8f2b9c89fd1
Daniel Saunders <94409438+dsaunders88@users.noreply.github.com>
2025-07-15 09:00:13 -0700

    fix document ids passthrough

--- src/_data/sanity/document_ids.js
+++ src/_data/sanity/document_ids.js
@@ -3,5 +3,5 @@
 export default {
     home: "c7f14479-088e-4b49-b696-d8329e066097",
     contact: "aa1fb61d-1a30-4ac7-a913-542739120533",
-    mainNav: "c7f14479-088e-4b49-b696-d8329e066097",
+    mainNav: "baa68fba-b096-4824-b989-ac581c189bca",
 };
